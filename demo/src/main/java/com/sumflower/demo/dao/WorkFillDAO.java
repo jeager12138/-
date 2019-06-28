e95bcdffc862d0ab7235486ab4eb43a42a954c25
WInjace <15811599533@163.com>
2019-06-28 18:37:39 +0800

    debug workFillDAO

--- demo/src/main/java/com/sumflower/demo/dao/WorkFillDAO.java
+++ demo/src/main/java/com/sumflower/demo/dao/WorkFillDAO.java
@@ -8,16 +8,16 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface WorkFillDAO {
     String TABLE_NAME = "Project";
-    String INSERT_FILEDS = " projectName,college,competitonType,studentName,studentNumber,birthDay,education,major," +
+    String INSERT_FILEDS = " projectName,college,competitionType,studentName,studentNumber,birthDay,education,major," +
             "entryYear,projectFullName,address,phone,email,friends,projectType,details,invention,keywords,picUrl," +
             "docUrl,videoUrl,averageScore,submitStatus " ;
 
 
-    @Insert({" insert into ", TABLE_NAME, "(", INSERT_FILEDS,
-    ") values ( #{projectName}, #{college}, #{competitonType}, #{studentName}, #{studentNumber}, #{birthDay}, " +
-            "#{education}, #{major}, #{entryYear}, #{projectFullName}, #{address}, #{phone}, #{email}," +
-            " #{friends}, #{projectType}, #{details}, #{invention}, #{keywords}, " +
-            "#{picUrl}, #{docUrl}, #{videoUrl}, #{averageScore}, #{submitStatus}"})
+    @Insert("insert into Project ( projectName,college,competitionType,studentName,studentNumber,birthDay,education,major,entryYear,projectFullName,address,phone,email,friends,projectType,details,invention,keywords,picUrl,docUrl,videoUrl,averageScore,submitStatus )\n" +
+            "values(#{projectName},\"\",#{competitionType},\"\",\"\",\n" +
+            "\t\"\",\"\",\"\",\"\",\"\",\n" +
+            "\t\"\",\"\",\"\",\"\",#{projectType},\n" +
+            "\t\"\",\"\",\"\",\"\",\"\",\n" +
+            "#{keywords},#{averageScore},#{submitStatus})")
     int addProject(Project project);
-
 }
