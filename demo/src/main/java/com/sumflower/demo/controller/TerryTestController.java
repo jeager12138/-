c44af29dd52fddd4865fb773cc85eff319791694
randerous <1521498542@qq.com>
2019-06-28 14:44:11 +0800

    local 6.28 14:43

--- demo/src/main/java/com/sumflower/demo/controller/TerryTestController.java
+++ demo/src/main/java/com/sumflower/demo/controller/TerryTestController.java
@@ -4,14 +4,35 @@ import com.sumflower.demo.model.StudentLogin;
 import com.sumflower.demo.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+
+@CrossOrigin
 @Controller
 public class TerryTestController {
 
     @Autowired
     StudentService studentService;
+
+    @RequestMapping(path = {"/api/post"})
+    @ResponseBody
+    public  Map post( @RequestBody Map map)
+    {
+        System.out.println(map.get("id"));
+        return  map;
+    }
+
+    @RequestMapping(path = {"/api/get"})
+    @ResponseBody
+    public  int  get( HttpServletRequest res)
+    {
+        System.out.println(res.getParameter("b"));
+        return  234348385;
+    }
+
 
     @RequestMapping(path = {"/testMysql"})
     @ResponseBody
