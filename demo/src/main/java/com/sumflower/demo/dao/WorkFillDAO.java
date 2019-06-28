77763c06478f91d4003d6b2d38935143e60de472
randerous <1521498542@qq.com>
2019-06-28 17:14:02 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	demo/.idea/workspace.xml
    #	demo/target/classes/com/sumflower/demo/controller/TerryTestController.class

--- demo/src/main/java/com/sumflower/demo/dao/WorkFillDAO.java
+++ demo/src/main/java/com/sumflower/demo/dao/WorkFillDAO.java
@@ -8,16 +8,16 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface WorkFillDAO {
     String TABLE_NAME = "Project";
-    String INSERT_FILEDS = "projectName,college,competitonType,studentName,studentNumber,birthDay,education,major," +
+    String INSERT_FILEDS = " projectName,college,competitonType,studentName,studentNumber,birthDay,education,major," +
             "entryYear,projectFullName,address,phone,email,friends,projectType,details,invention,keywords,picUrl," +
-            "docUrl,videoUrl,averageScore,submitStatus" ;
+            "docUrl,videoUrl,averageScore,submitStatus " ;
 
 
-    @Insert({"insert into ", TABLE_NAME, "(", INSERT_FILEDS,
-    ") values (#{projectName}, #{college}, #{competitonType}, #{studentName}, #{studentNumber}, #{birthDay}, " +
+    @Insert({" insert into ", TABLE_NAME, "(", INSERT_FILEDS,
+    ") values ( #{projectName}, #{college}, #{competitonType}, #{studentName}, #{studentNumber}, #{birthDay}, " +
             "#{education}, #{major}, #{entryYear}, #{projectFullName}, #{address}, #{phone}, #{email}," +
             " #{friends}, #{projectType}, #{details}, #{invention}, #{keywords}, " +
             "#{picUrl}, #{docUrl}, #{videoUrl}, #{averageScore}, #{submitStatus}"})
-    int addTicket(Project project);
+    int addProject(Project project);
 
 }
