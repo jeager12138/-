1cb73a22e0467da1549cdbb026caea82a4ac3463
TerryLiu <767432877@qq.com>
2019-06-29 09:01:07 +0800

    add configuration

---
+++ demo/src/main/java/com/sumflower/demo/configuration/WebConfiguration.java
@@ -0,0 +1,20 @@
+package com.sumflower.demo.configuration;
+
+import com.sumflower.demo.interceptor.PassportInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Component
+public class WebConfiguration extends WebMvcConfigurerAdapter {
+    @Autowired
+    PassportInterceptor passportInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(passportInterceptor);
+        super.addInterceptors(registry);
+    }
+
+}
