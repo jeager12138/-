cd4ece7b860c7c7d1fec4e516e1a57feb91b66ec
randerous <1521498542@qq.com>
2019-06-30 10:00:54 +0800

    test local upload: identifition + filename

--- demo/src/main/java/com/sumflower/demo/Controller/UploadFileController.java
+++ demo/src/main/java/com/sumflower/demo/Controller/UploadFileController.java
@@ -3,6 +3,10 @@ import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+
+import com.sumflower.demo.model.HostHolder;
+import com.sumflower.demo.model.LoginTicket;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -10,6 +14,8 @@ import org.springframework.web.multipart.MultipartFile;
 @CrossOrigin
 @Controller
 public class UploadFileController {
+    @Autowired
+    HostHolder hostHolder;
 
     @RequestMapping(path = "/upload", method = RequestMethod.GET)
     public String file() {
@@ -20,8 +26,22 @@ public class UploadFileController {
     @RequestMapping(path = "/upload", method = RequestMethod.POST)
     public String upload(@RequestParam("file") MultipartFile file) throws IOException {// 文件上传
 
+        LoginTicket loginTicket = hostHolder.getLoginTicket();
+        String filename;
+        if(loginTicket.getUserType() == 0)//student
+        {
+            filename = loginTicket.getUserId() + "_" + file.getOriginalFilename();
+        }else if(loginTicket.getUserType() == 2)//committee
+        {
+            filename = "committee" + "_" +file.getOriginalFilename();
+        }else
+        {
+            filename = file.getOriginalFilename();
+        }
+        //BufferedOutputStream outputStream = new BufferedOutputStream(new FileOutputStream(new File("/var/www/html/"+filename)));
+        //本地测试
         BufferedOutputStream outputStream = new BufferedOutputStream(
-                new FileOutputStream(new File("/var/www/html/"+file.getOriginalFilename())));
+                new FileOutputStream(new File(filename)));
         outputStream.write(file.getBytes());
         outputStream.flush();
         outputStream.close();
