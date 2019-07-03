9302601616bf2e281e74520b0982d80afeaabb87
TerryLiu <767432877@qq.com>
2019-07-03 16:52:11 +0800

    afternoon

--- demo/src/main/java/com/sumflower/demo/dao/WorkFillDAO.java
+++ demo/src/main/java/com/sumflower/demo/dao/WorkFillDAO.java
@@ -13,19 +13,28 @@ public interface WorkFillDAO {
             "entryYear,projectFullName,address,phone,email,friends,projectType,details,invention,keywords,picUrl," +
             "docUrl,videoUrl,averageScore,submitStatus " ;
 
-    @Insert({"insert into Project (studentId,submitStatus) values (#{studentId},1)"})
-    int createProject(int studentId);
+    @Insert({"insert into Project (studentId,competitionId,submitStatus) values (#{studentId},#{competitionId}, 1)"})
+    int createProject(Project project);
 
     @Update({"update Project set projectName = #{projectName},college = #{college},competitionType = 1," +
             "studentName = #{studentName},studentNumber = #{studentNumber},birthDay = #{birthDay},education = #{education}," +
             "major = #{major},entryYear = #{entryYear},projectFullName = #{projectFullName},address = #{address}," +
             "phone = #{phone},email = #{email},friends = #{friends},projectType = #{projectType},details = #{details}," +
             "invention = #{invention},keywords = #{keywords}," +
-            "submitStatus = #{submitStatus},studentId = #{studentId} where id = #{id}"})
+            "submitStatus = #{submitStatus},studentId = #{studentId},competitionId=#{competitionId} where id = #{id}"})
     int updateProject(Project project);
 
     @Update({"update Project set submitStatus = 0 where id = #{id}"})
     int updateProjectStatus(int id);
+
+    @Update({"update Project set submitStatus = 2 where id = #{id}"})
+    int passProject(int id);
+
+    @Update({"update Project set submitStatus = 3 where id = #{id}"})
+    int rejectProject(int id);
+
+    @Update({"update Project set submitStatus = 1 where id = #{id}"})
+    int giveBackProject(int id);
     /*
     @Insert({" insert into ", TABLE_NAME, "(", INSERT_FILEDS,
     ") values ( #{projectName}, #{college}, #{competitionType}, #{studentName}, #{studentNumber}, #{birthDay}, " +
@@ -40,7 +49,10 @@ public interface WorkFillDAO {
     @Select({"Select * From " , TABLE_NAME , " where id = #{id}"})
     Project getInfo(int id);
 
-    @Select({"Select * From " , TABLE_NAME , " where studentId = #{studentId}"})
-    List<Project> getWorkList(int studentId);
+    @Select({"Select * From " , TABLE_NAME , " where studentId = #{studentId} and competitionId=#{competitionId}"})
+    List<Project> getWorkList(@Param("studentId") int studentId, @Param("competitionId") int competitionId);
+
+    @Select({"Select * from ", TABLE_NAME, " where competitionId=#{competitionId} and submitStatus=0"})
+    List<Project> getAlreadyList(int competitionId);
 
 }
