8139b9564df9a0c85f41d5f301bf57b14b17d514
WInjace <15811599533@163.com>
2019-07-06 17:12:03 +0800

    返回项目列表时去除空项目

--- demo/src/main/java/com/sumflower/demo/Controller/WinjaceController.java
+++ demo/src/main/java/com/sumflower/demo/Controller/WinjaceController.java
@@ -1,19 +1,33 @@
 package com.sumflower.demo.Controller;
 
+import com.sumflower.demo.dao.WorkFillDAO;
 import com.sumflower.demo.model.Judge;
+import com.sumflower.demo.model.Project;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 @CrossOrigin
 @Controller
 public class WinjaceController {
+    @Autowired
+    WorkFillDAO workFillDAO;
 
     @RequestMapping(path = {"/api2"}, method = {RequestMethod.GET} )
     @ResponseBody
     public String api2(){
         return "test";
+    }
+
+    @RequestMapping(path = {"/api5"})
+    @ResponseBody
+    public List<Project> api5(@RequestBody Map m){
+        int id = Integer.parseInt((m.get("studentId")).toString());
+        int competitionId = 29;
+        List<Project> projectList = workFillDAO.getWorkList(id,competitionId);
+        return  projectList;
     }
 }
--- demo/src/main/java/com/sumflower/demo/dao/WorkFillDAO.java
+++ demo/src/main/java/com/sumflower/demo/dao/WorkFillDAO.java
@@ -49,7 +49,7 @@ public interface WorkFillDAO {
     @Select({"Select * From " , TABLE_NAME , " where id = #{id}"})
     Project getInfo(int id);
 
-    @Select({"Select * From " , TABLE_NAME , " where studentId = #{studentId} and competitionId=#{competitionId}"})
+    @Select({"Select * From " , TABLE_NAME , " where studentId = #{studentId} and competitionId=#{competitionId} and projectName is not null"})
     List<Project> getWorkList(@Param("studentId") int studentId, @Param("competitionId") int competitionId);
 
     @Select({"Select * from ", TABLE_NAME, " where competitionId=#{competitionId} and submitStatus!=0"})
